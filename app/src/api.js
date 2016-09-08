96cdd763827428b0da202121894a267710f2bbd0
IzumiSy <beetle-noise@gmx.com>
2016-09-08 17:25:53 +0900

    Implement some more skeltons

--- app/src/api.js
+++ app/src/api.js
@@ -1,5 +1,23 @@
 export default class ApiService {
-  static executeFind(query) {
+  constructor() {
+    // TODO this should be changed according to the env.
+    this.API_URL = 'http://localhost:3000/api/v1';
+    this.API_ENDPOINTS = {
+      find: '/pages/find',
+      index: '/pages'
+      show: '/pages'
+    }
+  }
+
+  executeFind(query) {
+    // TODO
+  }
+
+  executeIndex() {
+    // TODO
+  }
+
+  execiteShow(id) {
     // TODO
   }
 }
