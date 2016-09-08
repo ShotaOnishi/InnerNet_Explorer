c38cbae2e48127f687574a95fbe5de2e41bc3e0d
IzumiSy <beetle-noise@gmx.com>
2016-09-08 16:37:50 +0900

    Add Header component and ArticleList component

---
+++ app/src/components/ArticleList/ArticleList.vue
@@ -0,0 +1,7 @@
+<style scoped src="./ArticleList.scss"></style>
+<template src="./ArticleList.html"></template>
+<script>
+  export default {
+
+  }
+</script>
---
+++ app/src/components/Header/Header.vue
@@ -0,0 +1,7 @@
+<style scoped src="./Header.scss"></style>
+<template src="./Header.html"></template>
+<script>
+  export default {
+
+  }
+</script>
