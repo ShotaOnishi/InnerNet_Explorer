70c15db73d76f14190b16222e160fe875c4dbbbb
IzumiSy <beetle-noise@gmx.com>
2016-09-08 17:38:20 +0900

    Implement a skelton

--- app/src/components/Header/Header.vue
+++ app/src/components/Header/Header.vue
@@ -3,9 +3,31 @@
 <script>
   import ApiService from '../../api.js';
 
-  console.log(ApiService);
+  const apiService = new ApiService();
 
   export default {
+    data() {
+      return {
+        searchQuery: '',
+      };
+    },
 
+    watch: {
+      searchQuery: (() => {
+        this.executeSearch(this.searchQuery);
+      }),
+    },
+
+    ready() {
+      // TODO
+    },
+
+    methods: {
+      executeSearch(query) {
+        apiService.executeFind(query).then(result => {
+          // TODO
+        });
+      },
+    },
   };
 </script>
